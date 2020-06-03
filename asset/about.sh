56eaee23d979d93912224e166f4b1761408d498f
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2020-06-03 17:01:04 +0600

    Update about.sh

--- asset/about.sh
+++ asset/about.sh
@@ -1,12 +1,50 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+#Interface
+
+clear
+echo
+figlet CyberCat | lolcat
+echo
+echo BY : HADI ISLAM | lolcat
+echo
+date | lolcat
+echo
+echo $i" ABOUT US  " 
+echo
+echo
+echo $cy"Made By Hadi Islam"
+echo $i" Made In Bangladesh With Love ❤" 
+echo
+echo
+echo $i"#Use It At Your Own Risk."
+echo $i"#No Warranty."
+echo $i"#Use it legal purpose only."
+echo $i"#We are not responsible for your actions."
+echo $i"#Do not do things that are forbidden."
+echo $i"#If you are installing this tool."
+echo $i"#That means you are agree with all terms"
+echo
+echo $i":.:.:.:.:.:.:.:.:.:.:.:.:.:"
+echo
+read -p"Press 0 to back Homepage>>" pil
 
 
+#pill area
+
+if [ $pil = 0 ]
+then
+cd
+cd demo
+cd asset
+sh home.sh
+fi
 
 
-
-#Use It At Your Own Risk.
-#No Warranty.
-#Use it legal purpose only.
-#We are not responsible for your actions.
-#Do not do things that are forbidden.
-#If you are installing this tool,
-#that means you are agree with all terms
