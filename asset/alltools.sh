8ce9fd73aecdbd749a1bb037c27f5089f5c79571
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2020-06-03 14:26:32 +0600

    Update alltools.sh

--- asset/alltools.sh
+++ asset/alltools.sh
@@ -1,3 +1,13 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
 #Interface
 
 clear
