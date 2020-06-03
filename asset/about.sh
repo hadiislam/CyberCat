60119cb9d9add141329405e6011a3b416303ee28
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2020-06-03 16:51:54 +0600

    Update about.sh

--- asset/about.sh
+++ asset/about.sh
@@ -1,1 +1,12 @@
 
+
+
+
+
+#Use It At Your Own Risk.
+#No Warranty.
+#Use it legal purpose only.
+#We are not responsible for your actions.
+#Do not do things that are forbidden.
+#If you are installing this tool,
+#that means you are agree with all terms
