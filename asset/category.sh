30d207eba4e12c5c8836edd701c55eb3db27d93c
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2020-06-03 14:45:58 +0600

    Create category.sh

---
+++ asset/category.sh
@@ -0,0 +1,35 @@
+clear
+
+#color
+
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+clear
+
+#Interface
+
+figlet CyberCat | lolcat
+echo
+echo BY : HADI ISLAM | lolcat
+echo
+date | lolcat
+echo
+echo
+echo $i" 1. "$cy" Facebook Tools "
+echo
+echo $i" 2. "$cy" Phishing Tools "
+echo
+echo $i" 3. "$cy" Camera and Audio Hack"
+echo
+echo $i" 4. "$cy" Spam "
+echo
+echo $i" 0. "$cy" Back To Homepage "
+echo
+echo $i":.:.:.:.:.:.:.:.:.:.:.:.:.:.:.:"
+echo
+read -p"Input Number>> " pil
